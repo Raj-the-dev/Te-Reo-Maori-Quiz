c20f58989151fb77b95b1e346fd88bf43aad35d7
Raj <ac150547@avcol.school.nz>
2023-03-14 22:02:33 +1300

    information about this program is added to the Main Method. >>> Still some changes could be made to this method and the infromation that it hold.

--- Te Reo Maori/MaoriQuiz.cs
+++ Te Reo Maori/MaoriQuiz.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            
+            /*
+            Te-Reo-Maori-Quiz
+            The program will be a quiz based on Maori
+
+            What is Maori (language)? 
+            Maori is an Austronesian language primarily spoken in the North Island in New Zealand. 
+            Maori is not only the native language of New Zealand, but also the native people of New Zealand. Prior to European settlement in the 1800's, 
+            Maori was the only language spoken in NZ.
+
+            This project helps people learn Maori which is one of the most beautiful and well done language that everyone should know about.
+
+            By Giriraj Singh Rathore */
+
+            /*
+            1. User enters into the Program.
+            2. input name. If the input is bigger than 20 than ask user to enter nick name.
+            3. choose difficulty level. on that condition if the user does not input the right input then tell user to input the right difficulty.
+            4. Clear console and move to the quiz with the selected difficulty.
+            5. Answer quiz. 
+
+              a. If the Answer is invalid then repeat the instructions and tell user to input the answer again
+              b. If the Answer is wrong then display the answer and tell user to press "Enter" to mover to next question.
+              c. If the Answer is right then move to next question and increase the score by 10 points.
+              
+            6. Repeat the 5th step for 10 question.
+            7. Output the score and give a rewiew on their result. Ask the user whether he or she wants to re-do the quiz
+            */
+
+
         }
     }
 }
