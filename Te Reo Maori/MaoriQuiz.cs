b255b02cb5ece7286051bf96b23b09b6def29101
Raj <ac150547@avcol.school.nz>
2023-03-16 20:11:31 +1300

    Trying to fix issue in MENU method.
    issue : There is a format issue when the user does not enter anything.  Tried to fix it but couldn't.

--- Te Reo Maori/MaoriQuiz.cs
+++ Te Reo Maori/MaoriQuiz.cs
@@ -23,11 +23,12 @@ namespace Te_Reo_Maori
             5. Answer quiz.
 
               a. If the Answer is invalid then repeat the instructions and tell user to input the answer again
-              b. If the Answer is wrong then display the answer and tell user to press "Enter" to mover to next question.
+              b. If the Answer is wrong then display the answer. And update the question left to answer.Then tell user to press "Enter" to mover to next question.
               c. If the Answer is right then move to next question and increase the score by 10 points.
 
-            6. Repeat the 5th step for 10 question.
-            7. Output the score and give a rewiew on their result. Ask the user whether he or she wants to re-do the quiz
+            6. Repeat the 5th step for 5 question.
+            7. Output the score and give their result. 
+            8. Ask the user whether he or she wants to re-do the quiz
             */
 
             //About The Maori Quiz
@@ -41,7 +42,7 @@ namespace Te_Reo_Maori
             string About = "Welcome to the Maori Quiz! This quiz consists of multiple choice questions," +
                            " where you will be presented with a question and four possible answers: A, B, C, or D." +
                            " You will need to select the option that you believe is the correct answer to the question." +
-                           "\r\n\r\nTo answer a question, simply click on the option you believe is correct and press enter." +
+                           "\r\n\r\nTo answer a question, simply type the alphabet that belongs to the option that you believe is correct and press enter." +
                            " If you get the answer correct you will see the live score change on top right of the corner." +
                            " Else You will see the right answer next line. You can also see how many question your are left with also on the top right corner." +
                            "\r\n\r\nYou must answer all questions to complete the quiz." +
@@ -62,24 +63,27 @@ namespace Te_Reo_Maori
 
         static void Menu()
         {
-            string name, userchange = "";
+            string name = "", userchange = "";
             int userlevel = 0;
             
             Console.WriteLine("----------\nMaori Quiz\n----------");
 
-            Console.WriteLine("\nHow may I address you? (name)");
-            name = Console.ReadLine();
-            if (name.Length > 20)
+            while (name == "")
             {
-                Console.WriteLine(name + " please enter a nick name that you will want to use : ");
+                Console.WriteLine("\nPlease enter your name : ");
                 name = Console.ReadLine();
-            }
-
-            while (userlevel != 1 && userlevel != 2 && userlevel != 3 && userlevel != 4)
+            } 
+            
+            //FIX ISSUE if the user enters nothing.
+            while (userlevel != 1 && userlevel != 2 && userlevel != 3 && userlevel != 4 )
             {
                 Console.WriteLine("\nWhat is your desired Difficulty Level (Choose number) : ");
                 Console.WriteLine("1. Easier than easy (base)\n2. Beginner (Easy)\n3. Standard (Medium)\n4. Expert (Hard)\n");
                 userlevel = Convert.ToInt32(Console.ReadLine());
+                if (userlevel != 1 && userlevel != 2 && userlevel != 3 && userlevel != 4)
+                {
+                    Console.WriteLine($"\nInvalid input, {userlevel} is not a level.\n");
+                }
             }
 
 
@@ -107,7 +111,11 @@ namespace Te_Reo_Maori
 
         static void Base()
         {
-            
+            string quizheading = "----------Maori Quiz----------";
+            Console.SetCursorPosition((Console.WindowWidth - quizheading.Length) / 2, Console.CursorTop);
+            Console.WriteLine(quizheading);
+
+            Console.WriteLine();
         }
 
         static void Easy()
@@ -124,5 +132,6 @@ namespace Te_Reo_Maori
         {
 
         }
+        
     }
 }
