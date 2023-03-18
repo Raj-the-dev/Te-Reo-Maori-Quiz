9307a45bc384d50108f5cd2130556261e4632c55
Raj <ac150547@avcol.school.nz>
2023-03-18 20:30:02 +1300

    Started working on the easy, medium levels and did some work on base level. Fixed a bug in the medium and hard method

--- Te Reo Maori/MaoriQuiz.cs
+++ Te Reo Maori/MaoriQuiz.cs
@@ -35,7 +35,7 @@ namespace Te_Reo_Maori
             //About The Maori Quiz
             Info();
 
-            
+
         }
         static void Info()
         {
@@ -66,16 +66,17 @@ namespace Te_Reo_Maori
         {
             string name = "", strUserLevel;
             int userLevel = 0;
-            
+
             Console.WriteLine("----------\nMaori Quiz\n----------");
 
+            //Asking user for name
             while (name == "")
             {
                 Console.WriteLine("\nPlease enter your name : ");
                 name = Console.ReadLine();
-            } 
-            
-            
+            }
+
+            //Asking user for difficulty level
             while (userLevel != 1 && userLevel != 2 && userLevel != 3 && userLevel != 4 && userLevel != null)
             {
                 Console.WriteLine("\nWhat is your desired Difficulty Level (Choose number only) : ");
@@ -96,55 +97,59 @@ namespace Te_Reo_Maori
             Console.WriteLine("\n" + name + " please press Enter to Move on");
             Console.ReadKey();
             Console.Clear();
-
+            
+            //using the switch to change the method
             switch (userLevel)
             {
                 case 1:
                     Base(name);
                     break;
                 case 2:
-                    Easy();
+                    Easy(name);
                     break;
                 case 3:
-                    Medium();
+                    Medium(name);
                     break;
                 case 4:
-                    Hard();
+                    Hard(name);
                     break;
             }
         } //End of Main Method
-
+        
         static void Base(string name)
         {
+            //All variables in the method
             string quizHeading;
             string[] base_questions = new string[5];
             string[] base_answers = new string[5];
             string[] base_input = new string[5];
             int score = 0, questions_left = base_questions.Length;
 
+            //All 5 questions
             base_questions[0] = "\n\n1. What is the official language of New Zealand?" +
                                 "\nA. Japanese\nB. Maori\nC. Spanish\nD. French\n";
 
-            base_questions[1] = "\n\n2. What is the meaning of the Maori word \"kia ora\"?"+
+            base_questions[1] = "\n\n2. What is the meaning of the Maori word \"kia ora\"?" +
                                 "\nA. Goodbye\nB. Hello\nC. Thank you\nD. Welcome\n";
 
             base_questions[2] = "\n\n3. What is the name of the Maori New Year celebration?" +
                                 "\nA. Matariki\nB. Mardi Gas\nC. Cinco de Mayo\nD. None of the Above\n";
 
-            base_questions[3] = "\n\n4.What is the Maori work for \"Love\""+
+            base_questions[3] = "\n\n4.What is the Maori work for \"Love\"" +
                                 "\nA. Aroha\nB. Whakapapa\nC. Rongo\nD. Kai\n";
 
             base_questions[4] = "\n\nWhich of the following means \" Food\" in Maori" +
                                 "\nA. Mana\nB. Haka\nC. Kai\nD. None of the Above\n";
 
-            
+            //Answer for the questions
             base_answers[0] = "B";
             base_answers[1] = "B";
             base_answers[2] = "A";
             base_answers[3] = "A";
             base_answers[4] = "C";
 
-            for (int i = 0;i < base_questions.Length; i++)
+            //quiz for the Base level
+            for (int i = 0; i < base_questions.Length; i++)
             {
                 quizHeading = "----------Maori Quiz----------";
                 Console.SetCursorPosition((Console.WindowWidth - quizHeading.Length) / 2, Console.CursorTop);
@@ -161,12 +166,12 @@ namespace Te_Reo_Maori
                         Console.WriteLine("\nThe answer that you have entered is invalid. Please choose a option from the given question.\n");
                     }
                 }
-                
+
 
                 if (base_input[i] == base_answers[i])
                 {
                     Console.WriteLine("Right answer " + name);
-                    score+=2;
+                    score += 2;
                     questions_left--;
                 }
                 else
@@ -177,29 +182,161 @@ namespace Te_Reo_Maori
                 Console.WriteLine("\nPress Enter to continue");
                 Console.ReadKey();
                 Console.Clear();
-            }
-            
-           
+            }//end of loop
+
+
         } //End of Base Method
 
-        static void Easy()
+        static void Easy(string name)
+        {
+            //All variable in Easy level
+            string quizHeading;
+            string[] easy_questions = new string[5];
+            string[] easy_answers = new string[5];
+            string[] easy_input = new string[5];
+            int score = 0, questions_left = easy_questions.Length;
+
+            //Question for easy level
+            easy_questions[0] = "\n\n1. What is the official language of New Zealand?" +
+                                "\nA. Japanese\nB. Maori\nC. Spanish\nD. French\n";
+
+            easy_questions[1] = "\n\n2. What is the meaning of the Maori word \"kia ora\"?" +
+                                "\nA. Goodbye\nB. Hello\nC. Thank you\nD. Welcome\n";
+
+            easy_questions[2] = "\n\n3. What is the name of the Maori New Year celebration?" +
+                                "\nA. Matariki\nB. Mardi Gas\nC. Cinco de Mayo\nD. None of the Above\n";
+
+            easy_questions[3] = "\n\n4.What is the Maori work for \"Love\"" +
+                                "\nA. Aroha\nB. Whakapapa\nC. Rongo\nD. Kai\n";
+
+            easy_questions[4] = "\n\nWhich of the following means \" Food\" in Maori" +
+                                "\nA. Mana\nB. Haka\nC. Kai\nD. None of the Above\n";
+
+            //Answers for the question 
+            easy_answers[0] = "B";
+            easy_answers[1] = "B";
+            easy_answers[2] = "A";
+            easy_answers[3] = "A";
+            easy_answers[4] = "C";
+
+            //Easy quiz
+            for (int i = 0; i < easy_questions.Length; i++)
+            {
+                quizHeading = "----------Maori Quiz----------";
+                Console.SetCursorPosition((Console.WindowWidth - quizHeading.Length) / 2, Console.CursorTop);
+                Console.WriteLine(quizHeading);
+                Console.WriteLine(name + "'s score : " + score);
+                Console.WriteLine("Questions left : " + questions_left);
+
+                while (easy_input[i] != "A" && easy_input[i] != "B" && easy_input[i] != "C" && easy_input[i] != "D")
+                {
+                    Console.WriteLine(easy_questions[i]);
+                    easy_input[i] = Console.ReadLine().ToUpper();
+                    if (easy_input[i] != "A" && easy_input[i] != "B" && easy_input[i] != "C" && easy_input[i] != "D")
+                    {
+                        Console.WriteLine("\nThe answer that you have entered is invalid. Please choose a option from the given question.\n");
+                    }
+                }
+
+
+                if (easy_input[i] == easy_answers[i])
+                {
+                    Console.WriteLine("Right answer " + name);
+                    score += 2;
+                    questions_left--;
+                }
+                else
+                {
+                    Console.WriteLine("Nice try " + name);
+                    questions_left--;
+                }
+                Console.WriteLine("\nPress Enter to continue");
+                Console.ReadKey();
+                Console.Clear();
+
+            }//end of loop
+
+
+        }//End of Easy Method
+        static void Medium(string name)
+        {
+            //All variables the medium level
+            string quizHeading;
+            string[] medium_questions = new string[5];
+            string[] medium_answers = new string[5];
+            string[] medium_input = new string[5];
+            int score = 0, questions_left = medium_questions.Length;
+
+            //Questions for Medium level
+            medium_questions[0] = "\n\n1. What is the official language of New Zealand?" +
+                                "\nA. Japanese\nB. Maori\nC. Spanish\nD. French\n";
+
+            medium_questions[1] = "\n\n2. What is the meaning of the Maori word \"kia ora\"?" +
+                                "\nA. Goodbye\nB. Hello\nC. Thank you\nD. Welcome\n";
+
+            medium_questions[2] = "\n\n3. What is the name of the Maori New Year celebration?" +
+                                "\nA. Matariki\nB. Mardi Gas\nC. Cinco de Mayo\nD. None of the Above\n";
+
+            medium_questions[3] = "\n\n4.What is the Maori work for \"Love\"" +
+                                "\nA. Aroha\nB. Whakapapa\nC. Rongo\nD. Kai\n";
+
+            medium_questions[4] = "\n\nWhich of the following means \" Food\" in Maori" +
+                                "\nA. Mana\nB. Haka\nC. Kai\nD. None of the Above\n";
+
+            //Answers for the medium level
+            medium_answers[0] = "B";
+            medium_answers[1] = "B";
+            medium_answers[2] = "A";
+            medium_answers[3] = "A";
+            medium_answers[4] = "C";
+
+
+            //Medium quiz
+            for (int i = 0; i < medium_questions.Length; i++)
+            {
+                quizHeading = "----------Maori Quiz----------";
+                Console.SetCursorPosition((Console.WindowWidth - quizHeading.Length) / 2, Console.CursorTop);
+                Console.WriteLine(quizHeading);
+                Console.WriteLine(name + "'s score : " + score);
+                Console.WriteLine("Questions left : " + questions_left);
+
+                while (medium_input[i] != "A" && medium_input[i] != "B" && medium_input[i] != "C" && medium_input[i] != "D")
+                {
+                    Console.WriteLine(medium_questions[i]);
+                    medium_input[i] = Console.ReadLine().ToUpper();
+                    if (medium_input[i] != "A" && medium_input[i] != "B" && medium_input[i] != "C" && medium_input[i] != "D")
+                    {
+                        Console.WriteLine("\nThe answer that you have entered is invalid. Please choose a option from the given question.\n");
+                    }
+                }
+
+
+                if (medium_input[i] == medium_answers[i])
+                {
+                    Console.WriteLine("Right answer " + name);
+                    score += 2;
+                    questions_left--;
+                }
+                else
+                {
+                    Console.WriteLine("Nice try " + name);
+                    questions_left--;
+                }
+                Console.WriteLine("\nPress Enter to continue");
+                Console.ReadKey();
+                Console.Clear();
+
+            }//end of loop
+
+
+        }//End of medium method
+        static void Hard(string name)
         {
 
-        } //End of Easy Method
-
-        static void Medium()
-        {
-
-        } //End of Medium Method
-
-        static void Hard()
-        {
-
-        } //End of Hard Method
-        
+        }
         static void Results()
         {
 
-        } // End of Results Method
+        }
     }
 }
